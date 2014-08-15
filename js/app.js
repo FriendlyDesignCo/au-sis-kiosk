8beea1f6dd2077debff61bae76fd1744c300b0b4
Ross Nover <ross@friendlydesign.co>
2014-08-15 14:47:10 -0400

    File dump.

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// Foundation JavaScript
+// Documentation can be found at: http://foundation.zurb.com/docs
+
+$(document).foundation();
+//  $(document).foundation({
+//   magellan : {
+//     destination_threshold: 100
+//   }
+// });
